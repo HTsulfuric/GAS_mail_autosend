db8e54834b926b1316f2fafb07a41ff37e5395ee
HTsulfuric <leo.phoenix2003@gmail.com>
2024-09-28 13:01:12 +0900

    initial commit

---
+++ autosend.js
@@ -0,0 +1,42 @@
+function onOpen() {
+  var ui = SpreadsheetApp.getUi();
+  ui.createMenu("Auto Send").addItem("Send Emails", "sendEmails").addToUi();
+}
+
+function sendEmails() {
+  const ss = SpreadsheetApp.getActive();
+
+  const setting = ss.getSheetByName("setting");
+  const list = ss.getSheetByName(setting.getRange(2, 1).getValue());
+
+  const lastRow = list.getLastRow();
+
+  const title = setting.getRange(2, 2).getValue();
+
+  const documentUrl = setting.getRange(2, 3).getValue();
+
+  const openDoc = DocumentApp.openByUrl(documentUrl);
+  let body = openDoc.getBody().getText();
+
+  let file = [];
+  if (attachBool == "Yes") {
+    for (let i = 4; i <= 6; i++) {
+      if (setting.getRange(2, i).getValue() != "") {
+        let fileId = setting.getRange(2, i).getValue().split("/")[5];
+        let blob = DriveApp.getFileById(fileId).getBlob();
+        file.push(blob);
+      }
+    }
+  }
+
+  for (let i = 2; i <= lastRow; i++) {
+    let address = list.getRange(i, 1).getValue();
+    let name = list.getRange(i, 2).getValue();
+
+    let emailBody = body.replace("{{name}}", name);
+
+    GmailApp.sendEmail(address, title, emailBody, {
+      attachments: file,
+    });
+  }
+}
